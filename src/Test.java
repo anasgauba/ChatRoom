93a90c2358a8cb5b102c6ca32a5cfc80660a0239
Anas Gauba <anas.farooq71@gmail.com>
2018-09-28 14:33:06 -0600

    Finished naive implementation of tupleSpace using linkedList data struct

--- src/Test.java
+++ src/Test.java
@@ -18,44 +18,55 @@ public class Test {
 //        calendar.setTimeInMillis(time);
 //        System.out.println(calendar.get(Calendar.HOUR_OF_DAY) + ":" +
 //                calendar.get(Calendar.MILLISECOND));
-//        System.out.println(time / 1000);
-        for (int i = 0; i < 100000; i++) {
+        System.out.println(time);
+        for (int i = 0; i < 10000; i++) {
             if (i % 2 == 0) {
+//                System.out.println("i mod 2 " + i);
                 tuple = new Tuple("anas " + i, 2);
                 trie.add(tuple);
             }
             else if (i % 3 == 0) {
+//                System.out.println("i mod 3 " + i);
                 tuple = new Tuple( 2.54, 2.678884224, "pet " + i);
                 trie.add(tuple);
             }
             else if (i % 5 == 0) {
+//                System.out.println("i mod 5 " + i);
                 tuple = new Tuple('d', 3.1458, 1.98643320, 'm', "dog " + i);
                 trie.add(tuple);
             }
             else if (i % 7 == 0) {
+//                System.out.println("i mod 7 " + i);
                 tuple = new Tuple(false, 3.1458, 1.945343320, 'b', "dog " + i);
                 trie.add(tuple);
             }
-            else if (i % 29 == 0) {
+            else if (i % 25 == 0) {
+//                System.out.println("i mod 29 " + i);
                 tuple = new Tuple(false, "str " + true, 1.98643320, 'm', "dog " +
                         i, i);
                 trie.add(tuple);
             }
             else if (i % 77 == 0) {
+//                System.out.println("i mod 77 " + i);
                 tuple = new Tuple(false, "str " + true, 1.98643320, 'c',
                         "dog " +
                         i, i, 10.0 + i);
                 trie.add(tuple);
             }
             else {
+//                System.out.println("i mod else " + i);
                 tuple = new Tuple("ana ", 'v', 22.00 + i, 2500 + "bob " + i,
                         true, 2.777777444331111, 2*i, "bob the " + 4*i);
                 trie.add(tuple);
             }
         }
-//        System.out.println(trie.getMapSize());
+        long time2 = System.currentTimeMillis();
 
-//        trie.search("anas " + 4, 2, 5);
+//        System.out.println(trie.getMapSize());
+        System.out.println(time2 - time + " millis");
+
+//        trie.remove("anas " + 4, null);
+        trie.search("anas " + 4, 2);
 //        trie.search('c', 2.54, 2.678884224, null);
 //        trie.remove("anas " + 4, 2, 5);
     }
--- src/Tuple.java
+++ src/Tuple.java
@@ -1,31 +1,50 @@
 /**
- * A class for tuple of any object (String, char, etc).
- * Have methods to get size and get the specific object
+ * A class for tuple of any pattern (String, char, etc).
+ * Have methods to get size and get the specific pattern
  * at a particular index.
  * @version date: 2018-09-20
  * @author Anas Farooq Gauba
  */
 public class Tuple {
 
-    private Object[] object;
+    private Object[] pattern;
 
     /**
      * Construct a tuple obj and add it to
      * the obj array.
-     * @param objs any object type
+     * @param objs any pattern type
      */
     public Tuple(Object... objs) {
-        this.object = objs;
+        this.pattern = objs;
+    }
+    public boolean isEqual(Tuple t1) {
+        Object[] temp = t1.getPattern();
+
+        if (t1.getSize() != this.getSize()) {
+            return false;
+        }
+        else {
+            for (int i = 0; i < getSize(); i++) {
+                if (!pattern[i].equals(temp[i])&& temp[i] != "*") {
+                    return false;
+                }
+            }
+
+        }
+        return true;
     }
 
     /**
-     * Gets any object at specific index in the object
+     * Gets any pattern at specific index in the pattern
      * array.
      * @param i index
-     * @return specific object at specific index
+     * @return specific pattern at specific index
      */
     public Object get(int i) {
-        return object[i];
+        return pattern[i];
+    }
+    public Object[] getPattern() {
+        return pattern;
     }
 
     /**
@@ -33,13 +52,13 @@ public class Tuple {
      * @return the length of obj array.
      */
     public int getSize() {
-        return object.length;
+        return pattern.length;
     }
 
     public String toString() {
         String str = "[ ";
-        for (int i = 0; i < object.length; i++) {
-            str += object[i] + " ";
+        for (int i = 0; i < pattern.length; i++) {
+            str += pattern[i] + " ";
         }
         str += "]";
         return str;
